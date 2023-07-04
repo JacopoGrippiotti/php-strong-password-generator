fd661878f9004845fb268658463c699b58ba5dd1
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-04 17:22:36 +0200

    fixed style of index.php file

--- index.php
+++ index.php
@@ -9,17 +9,18 @@
  </head>
  <body>
     <?php include __DIR__ . '/utilities/functions.php' ?>
-      <h1 class='ms-5'><?php  if(!empty($_GET['passwordLength'])){echo generate_pw($_GET['passwordLength']);}else{echo "insert length";}?></h1>
-    <form action="./index.php" method="get" class="m-4">
-        <input type="number" id="password" name="passwordLength" placeholder="password">
-        <input type="submit">
-    </form>
-
- 
-
-
-
-
+    <div class="password-container">
+        <div>
+            <h1>Strong Password Generator</h1>
+            <h2 ><?php  if(!empty($_GET['passwordLength'])){echo generate_pw($_GET['passwordLength']);}else{echo "insert length";}?></h2>
+            <form action="./index.php" method="get">
+                <input type="number" id="password" name="passwordLength" placeholder="password">
+                <input type="submit">
+            </form>   
+        </div>
+      
+    </div>
+    
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
  </body>
 </html>
